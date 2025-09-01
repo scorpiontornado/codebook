2b3075acf4c0e956af735ec92e399da60265a93c
Nicholas Langford <dev@nlangford.au>
2025-09-02 01:02:33 +1000

    Add basic template

---
+++ template.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pq_min = priority_queue<T, vector<T>, greater<>>;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)    // half-open: [a, b)
+#define repi(i, a, b) for (int i = (a); i <= (b); ++i)  // inclusive: [a, b]
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb emplace_back
+
+const int INF = 1e9;  // numeric_limits<int>::max();
+const long long LINF = 4e18;
+const double EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+
+    int t = 1;
+    // std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
