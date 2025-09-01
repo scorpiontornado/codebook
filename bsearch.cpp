b4bdf1269904972b91f92e00ed3dfd79eb32b730
Nicholas Langford <dev@nlangford.au>
2025-09-01 22:07:19 +1000

    Add binary search from UNSW COMP4128 lecture code

--- bsearch.cpp
+++ bsearch.cpp
@@ -1,4 +1,25 @@
 // Binary search. This is included because binary search can be tricky.
+
+#include <functional>;
+using namespace std;
+
+// Find the smallest X such that f(X) is true
+// from UNSW COMP4128 lecture code, topic 3. Paradigms
+int binarysearch(function<bool(int)> f) {
+    int lo = 0, hi = 100000, bestSoFar = -1;
+    // Range [lo, hi];
+    while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if (f(mid)) {
+            bestSoFar = mid;
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return bestSoFar;
+}
+
 // n is size of array A, c is value we're searching for. Semantics follow those of std::lower_bound and std::upper_bound
 int lower_bound(int A[], int n, int c) {
     int l = 0;
@@ -8,8 +29,8 @@ int lower_bound(int A[], int n, int c) {
         if (A[m] < c) l = m+1; else r = m;
     }
     return l;
+}
 
- 
 int upper_bound(int A[], int n, int c) {
     int l = 0;
     int r = n;
